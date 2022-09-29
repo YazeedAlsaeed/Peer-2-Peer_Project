d81b92169685faac269f0036ffac95670b77f7f6
AbdullahAXR <74458970+AbdullahAXR@users.noreply.github.com>
2022-09-30 01:07:27 +0300

    Change Hanldle to class but I did not change the implementation of follow and drop

--- Socket_Project.py
+++ Socket_Project.py
@@ -1,25 +1,38 @@
 import socket
 
-#Form the handles dictionary
-handles = {"":("",[""])}
-
-#register the handle if unique
-def register(handle,ipv4,port):
+class handle():
+    def __init__(self,handleName,ipv4,port1,port2,port3):
+        self.handleName = handleName
+        self.ipv4 = ipv4
+        self.port1 = port1
+        self.port2 = port2
+        self.port3 = port3
+        slef.followers = []
     
-    for i in handles.keys():
-        if handle == i:
-            return("FAILURE")
-        else : 
-            handles[handle] = (ipv4+":"+port,[])
-            return("SUCCESS")
+#Form the handles dictionary
+handles = []
+#register the handle if unique
+def register(handleName,ipv4,port1,port2,port3):
+    for i in handles:
+        if handleName == i.handleName:
+            return "FAILURE"
+    handleA = handle(handleName,ipv4,port1,port2,port3)
+    handles.append(handleA)
+    return "SUCCESS"
+            
 
 #Return the number of handles with its content
 def query_handles():
-    handles.pop("")
-    return(len(handles),list(handles.keys()))
+    listOfHandlesNames = [i.handleName for i in handles]
+    return(len(handles),listOfHandlesNames)
 
 #The ability for handle A to follow handle B
 def follow(handleA,handleB):
+    indexOfHandleB = find(handleB)
+    handles[indexOfHandleB].followers.append(handleA)
+    handles[indexOfHandleB].followers.sort()
+    
+    
     followers = handles[handleB][1]
     followers.append(handleA)
     followers.sort()
@@ -33,20 +46,28 @@ def drop(handleA,handleB):
     followers.sort()
     ip = handles[handleB][0]
     handles[handleB] = (ip, followers)
-
+def find(handleName):
+    count=0
+    for i in handles:
+        if handleName == i.handleName:
+            return count:
+        count++
+    return -1:
 
 #Test
-print(register("khalid","4","4"))
+print(register("khalid","4","1","2","3"))
+print(register("khalid2","4","1","2","3"))
 #print(register("moh",4,4))
 #register("zezo",4,4)
 #register("moath",4,4)
 #register("abrar",4,4)
 print(query_handles())
 
-follow("salih","khalid")
-follow("moath","khalid")
-follow("abrar","khalid")
-follow("zezo","khalid")
-drop("zezo","khalid")
-
-print(handles["khalid"])
+#follow("salih","khalid")
+#follow("moath","khalid")
+#follow("abrar","khalid")
+#follow("zezo","khalid")
+#drop("zezo","khalid")
+#print(handles["khalid"])
+print(handles)
+print(handles[0].handleName)
