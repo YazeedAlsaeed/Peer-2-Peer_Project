bb4e3ff53c6f4b70e08ac1759edff79dd3332a03
YazeedAlsaeed <97403543+YazeedAlsaeed@users.noreply.github.com>
2022-10-21 17:20:26 -0700

    Update Trecker.py

--- Trecker.py
+++ Trecker.py
@@ -1,15 +1,12 @@
 import socket
 import threading
-import sys
-import pickle
-
 
 #Defines the varaibles
 HEADER = 1024
-SERVER = socket.gethostbyname(socket.gethostname())
 PORT = 36500
-ADDR = (SERVER,PORT)
+ADDR = ("",PORT)
 FORMAT = "utf-8"
+cond = False
 
 #Define the socket
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -41,37 +38,46 @@ def handle_client(conn,addr):
         elif msg[0] == "2":
             message = msg.split()
             if len(message) != 2:
-                conn.send("Error".encode(FORMAT))
+                conn.send("out : Error".encode(FORMAT))
             else:
                 handlesOfFollowing = find(message[1])
                 if(not handlesOfFollowing):
-                    conn.send(("Error: The user can not be found").encode(FORMAT))
+                    conn.send(("out : Error: The user can not be found").encode(FORMAT))
                 else:
                     follow(obj,handlesOfFollowing)
-                    conn.send(("Followed Succecfully").encode(FORMAT)) 
+                    conn.send(("out : Followed Succecfully").encode(FORMAT)) 
         
         #To drop other handles    
         elif msg[0] == "3":
             message = msg.split()
             if len(message) != 2:
-                conn.send("Error".encode(FORMAT))
+                conn.send("out : Error".encode(FORMAT))
             else:
                 handlesOfDropped = find(message[1])
                 if(not handlesOfDropped):
-                    conn.send(("Error: The user can not be found").encode(FORMAT))
+                    conn.send(("out : Error: The user can not be found").encode(FORMAT))
                 else:
                     try:
                         drop(obj,handlesOfDropped)
-                        conn.send(("Dropped Succecfully").encode(FORMAT))
+                        conn.send(("out : Dropped Succecfully").encode(FORMAT))
                     except:
                         conn.send(("Error has occured").encode(FORMAT))
-             
+        #To tweet 
+        elif msg[0] == "4":
+            message = msg.split()
+            if len(message) != 2:
+                conn.send("out : Error 'Takes one argument only'".encode(FORMAT))
+            else:
+                conn.send(tweet(message[1]).encode(FORMAT))
+
          #To exit   
         elif msg[0] == "0":
             connected = False
             message = exit(obj)
             conn.send(message.encode(FORMAT))
     print("\nCONNECTION WITH " + obj.ipv4 + ":" + obj.port1 + " CLOSED")
+    if len(handles) == 0:
+        cond = True
     conn.close()
     
 
@@ -80,15 +86,12 @@ def handle_client(conn,addr):
 def start():
     count = 1
     server.listen()
-    print("Server " + SERVER + " is listening now ...")
+    print("Tracker is listening now ...")
     while True:
        conn, addr = server.accept()
        thread = threading.Thread(target=handle_client, args=(conn,addr))
        thread.start()
        print("ACTIVE CONNECTION THREAD NUMBER " + str(count))
-       if len(handles) == 0:
-        print("Since there is no connection, The program has terminated :)")
-        sys.exit()
        count += 1
         
        
@@ -112,19 +115,18 @@ handles = []
 def register(object):
     for i in handles:
         if object.handleName == i.handleName:
-            return "FAILURE"
+            return "out : FAILURE"
     handles.append(object)
-    return "SUCCESS"
+    return "out : SUCCESS"
             
 #Return the number of handles with its content
 def query_handles():
     listOfHandlesNames = [i.handleName for i in handles]
-    return(str(len(handles)) + " , " + str(listOfHandlesNames))
+    return("out : " + str(len(handles)) + " , " + str(listOfHandlesNames))
 
 #The ability for handle A to follow handle B
 def follow(handleA,handleB):
     handleB.followers.append(handleA)
-    handleB.followers.sort()
 
     #For exit function only
     handleA.following.append(handleB)
@@ -134,12 +136,21 @@ def drop(handleA,handleB):
     handleB.followers.remove(handleA)
     handleA.following.remove(handleB)
 
+#The ability for a spesific handle to tweet 
+def tweet(handle):
+    for i in handles:
+        if i.handleName == handle:
+            return buildLogic(handle)
+    return "FAILURE"
+
+#TODO end-tweet
+
 #Exit handle A from the proccess
 def exit(handle):
     for i in handle.following:
         drop(handle,i)
     handles.remove(handle)
-    return handle.handleName + " exit succeccfully"
+    return handle.handleName + "out : exit succeccfully"
 
 #To read a string from user and find the corresponding object to it
 def find(handleName):
@@ -148,6 +159,13 @@ def find(handleName):
             return i
     return 0
 
+#To build a logic ring table
+def buildLogic(handle):
+    followers = find(handle).followers
+    table = ""
+    for i in followers:
+        table += " " + str(i.ipv4 + "," + i.port2 + "," + i.port3)
+    return table
 
 #Start the thread and proccess
 start()
